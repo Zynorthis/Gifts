5247e65143d5c96360e6c19fc38acfeb18ab3827
Jacob Taylor <jacobtaylor727@outlook.com>
2025-01-10 16:51:02 -0600

    feat: Added more placeholder navbar links.

--- gifts/src/NavigationBar/NavigationBar.tsx
+++ gifts/src/NavigationBar/NavigationBar.tsx
@@ -6,8 +6,18 @@ export const Navigationbar = () => {
         <ul>
             <LinkListItem link="https://jacobt.dev/" text="Portfolio" />
         </ul>
+        <ul className='divider'>|</ul>
         <ul>
             <LinkListItem link='https://www.amazon.com/hz/wishlist/ls/1BUI9J1LZVV5S?ref_=wl_share' text='Amazon WishList' />
+        </ul>
+        <ul>
+            <LinkListItem link='#' text='Home Automation' />
+        </ul>
+        <ul>
+            <LinkListItem link='#' text='D&D' />
+        </ul>
+        <ul>
+            <LinkListItem link='#' text='Board Games' />
         </ul>
     </nav>
 };
