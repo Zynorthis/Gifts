733c4b8c623d265fe5b997e6330e5795ed3905d4
Jacob Taylor <jacobtaylor727@outlook.com>
2025-01-10 16:36:35 -0600

    style: Added link back to portfolio in navbar. Updated navbar styles. Updated landing page styles.

--- gifts/src/LandingPage.tsx
+++ gifts/src/LandingPage.tsx
@@ -34,7 +34,8 @@ export const LandingPage = () => {
     return (<>
         <Navigationbar />
         <h1>Gift Registry</h1>
-        <p>I got tired of sending out the same gift list year round, so here's my permanent gift registry (up to date prices and not owned yet)</p>
+        <p>I got tired of sending out the same gift list year round, so here's my permanent gift registry</p>
+        <p className="subtext">(up to date prices and not owned yet)</p>
         {underFiftyGifts && (
             <>
                 <h3>Under $50</h3>
--- gifts/src/NavigationBar/NavigationBar.tsx
+++ gifts/src/NavigationBar/NavigationBar.tsx
@@ -4,6 +4,9 @@ import './NavigationBar.css';
 export const Navigationbar = () => {
     return <nav>
         <ul>
+            <LinkListItem link="https://jacobt.dev/" text="Portfolio" />
+        </ul>
+        <ul>
             <LinkListItem link='https://www.amazon.com/hz/wishlist/ls/1BUI9J1LZVV5S?ref_=wl_share' text='Amazon WishList' />
         </ul>
     </nav>
