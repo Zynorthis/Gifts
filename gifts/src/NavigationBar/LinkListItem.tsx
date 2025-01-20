d589da3b017a05e572c2497959b4d2bf1659be5d
Zynorthis-Main-Desktop <Zynorthis@gmail.com>
2025-01-19 23:08:07 -0600

    ci: Fixed noreferrer security vulnerablity warning. Yarn lock update.

--- gifts/src/NavigationBar/LinkListItem.tsx
+++ gifts/src/NavigationBar/LinkListItem.tsx
@@ -7,6 +7,6 @@ export interface LinkListItemProps {
 
 export const LinkListItem = ({ link, text }: LinkListItemProps) => {
     return <li>
-        <a href={link} target='_blank'>{text}</a>
+        <a href={link} target='_blank' rel='noreferrer'>{text}</a>
     </li>
 };
